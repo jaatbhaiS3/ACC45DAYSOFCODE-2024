247b69054f652460842bb5f0dd35b5ed74bc545e
jaatbhaiS3 <jats11851@gmail.com>
2024-10-01 10:06:16 +0530

    Add files via upload

---
+++ Day-4,THREETOPICS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int a,b,c,x;
+        scanf("%d %d %d %d",&a,&b,&c,&x);
+        if(x==a || x==b || x==c)
+        printf("YES \n");
+        else
+        printf("NO \n");
+       
+        
+	// your code goes here
+	return 0;
+}
